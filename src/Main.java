d00aad61f20a442fbde54fe2b86bdb394952f497
Utilisateur <stephane.rodolausse.pf1@gmail.com>
2018-04-03 13:35:40 +0200

    Adding try catch

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
+	public static Scanner in = new Scanner(System.in);
+
 	// Écrire une fonction qui affiche si une année est bissextile ou non.
 	// Écrire une fonction qui affiche combien d'années dans un intervalle donné
 	// (par exemple [1980, 2016[ ) sont bissextiles.
@@ -12,28 +14,68 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner in = new Scanner(System.in);
+		boolean endOfProg = false;
+		String answer = "";
 
-		int startDate;
-		int endDate;
+		do {
+			System.out.println("Voulez vous connaître les années bisextiles entre 2 dates?");
+			System.out.println("0) Non, sortir du programme." + "\n" + "1) Oui.");
 
-		System.out.println("Voulez vous connaître les années bisextiles entre 2 dates?");
-		System.out.println("Donnez la date de départ");
-		startDate = in.nextInt();
-		in.nextLine();
-		System.out.println("Donnez la date de fin");
-		endDate = in.nextInt();
-		in.nextLine();
+			answer = in.nextLine();
 
-		dateIsBisextile(startDate, endDate);
-		
+			switch (answer)
+
+			{
+
+			case "1":
+				input();
+				break;
+
+			case "0":
+				System.out.println("Bonne journée");
+				endOfProg = true;
+				break;
+
+			default:
+				System.out.println("Donnez une réponse correcte");
+			}
+		} while (!endOfProg);
+
 		in.close();
 
 	}
 
+	/*
+	 * boolean bError = true; int n1 = 0, n2 = 0, nQuotient = 0;
+	 * 
+	 * do { try { System.out.println("Enter first num: "); n1 = input.nextInt();
+	 * System.out.println("Enter second num: "); n2 = input.nextInt(); nQuotient =
+	 * n1/n2; bError = false; } catch (Exception e) { System.out.println("Error!");
+	 * } } while (bError);
+	 */
+	public static void input() {
+
+		int startDate;
+		int endDate;
+		try {
+			System.out.println("Donnez la date de départ");
+			startDate = in.nextInt();
+			System.out.println("Donnez la date de fin");
+			endDate = in.nextInt();
+			
+			dateIsBisextile(startDate, endDate);
+		}
+
+		catch (Exception e) {
+			System.out.println("Erreur de saisie !");
+
+		}
+
+	}
+
 	private static void dateIsBisextile(int sDate, int eDate) {
-		boolean stop = false;	
-		
+		boolean stop = false;
+
 		List<Integer> years = new ArrayList<Integer>();
 		do {
 			if (sDate <= eDate && sDate > 0) {
@@ -45,7 +87,7 @@ public class Main {
 						years.add(i);
 
 					}
-					
+
 					stop = true;
 				}
 
@@ -57,10 +99,10 @@ public class Main {
 		} while (!stop);
 
 		System.out.println("voici les années bisextiles entre " + sDate + " et " + eDate);
-		for (int j=0; j< years.size(); j++) {
-			
+		for (int j = 0; j < years.size(); j++) {
+
 			System.out.println(years.get(j));
-			}
+		}
 	}
 
 }
